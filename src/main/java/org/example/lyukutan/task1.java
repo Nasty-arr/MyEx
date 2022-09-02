d5fe11627f55cf755002dae16a3a52638d152c1a
Tarakanova Anastasia <shamordin.sv@reliab.tech>
2022-09-02 09:45:56 +0300

    добавила первую задачу люка

--- src/main/java/org/example/lyukutan/task1.java
+++ src/main/java/org/example/lyukutan/task1.java
@@ -1,4 +1,16 @@
 package org.example.lyukutan;
-
+import java.io.PrintStream;
+import java.util.Scanner;
 public class task1 {
+    public static void main(String[] args ){
+        Scanner console = new Scanner(System.in);
+        int first = console.nextInt();
+        int second = console.nextInt();
+        int third = console.nextInt();
+        PrintStream result = first == second && first == third ?
+                System.out.printf("%d, %d, %d", first,second,third) : first == second  ?
+                System.out.printf("%d, %d",first,second) : first == third ?  System.out.printf("%d, %d",first,third) : second == third ?
+                System.out.printf("%d, %d",second, third): System.out.printf("Нет дубликатов");
+        System.out.println(result);
+    }
 }
